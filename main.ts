6d986c4ea9e60f6bab9bd33c1d13a7f66aeca068
Alexandro Aguilar <auza920205@gmail.com>
2020-09-13 10:50:30 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -16,44 +16,45 @@ let pressure = 0
 let humidity = 0
 let temperature = 0
 let signal = ""
+let json = ""
 weatherbit.startWeatherMonitoring()
-serial.redirectToUSB()
-basic.forever(function () {
-    signal = serial.readLine()
-    temperature = weatherbit.temperature() / 100
-    humidity = weatherbit.humidity() / 1024
-    pressure = weatherbit.pressure() / 25600
-    altitude = weatherbit.altitude()
-    windSpeed = weatherbit.windSpeed()
-    windDirection = weatherbit.windDirection()
-    serial.writeValue("temperature:", temperature)
-    serial.writeValue(",humidity:", humidity)
-    serial.writeValue(",pressure:", pressure)
-    serial.writeValue(",altitude:", altitude)
-    serial.writeValue(",wind speed:", windSpeed)
-    serial.writeString(",wind direction:" + windDirection)
-    led.setBrightness(10)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . # . .
-        . . . . .
-        . . . . .
-        `)
-    basic.showLeds(`
-        . . . . .
-        . # # # .
-        . # . # .
-        . # # # .
-        . . . . .
-        `)
-    basic.showLeds(`
-        # # # # #
-        # . . . #
-        # . . . #
-        # . . . #
-        # # # # #
-        `)
-    basic.clearScreen()
+
+basic.forever( () => {
+    //signal = serial.readLine()
+    serial.onDataReceived(serial.delimiters(Delimiters.NewLine), function () {
+        temperature = weatherbit.temperature() / 100
+        humidity = weatherbit.humidity() / 1024
+        pressure = weatherbit.pressure() / 25600
+        altitude = weatherbit.altitude()
+        windSpeed = weatherbit.windSpeed()
+        windDirection = weatherbit.windDirection()
+        json = "{temperature:" + temperature + ",humidity:" + humidity + ",pressure:" + pressure + ",altitude:" + altitude + ",windSpeed:" + windSpeed + ",windDirection:" + windDirection + "}"
+        serial.writeLine(json)
+        led.setBrightness(10)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # . # .
+            . # # # .
+            . . . . .
+            `)
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+        basic.clearScreen()
+    })
+    
+    //basic.pause(2000)
 })
 
