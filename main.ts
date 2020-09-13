1069cd41eee2ea890459b6ad08091881aa51b1b5
Alexandro Aguilar <auza920205@gmail.com>
2020-09-12 22:36:20 -0500

    more sensors added

--- main.ts
+++ main.ts
@@ -1,14 +1,37 @@
+/**
+ * serial.redirect(
+ * 
+ * SerialPin.P15,
+ * 
+ * SerialPin.P14,
+ * 
+ * BaudRate.BaudRate9600
+ * 
+ * )
+ */
+let windDirection = ""
+let windSpeed = 0
+let altitude = 0
+let pressure = 0
+let humidity = 0
+let temperature = 0
+let signal = ""
 weatherbit.startWeatherMonitoring()
+serial.redirectToUSB()
 basic.forever(function () {
-    let signal: string = serial.readLine()
-    let temperature: number = weatherbit.temperature() / 100
-    let humidity: number = weatherbit.humidity() / 1024
-    let pressure: number = weatherbit.pressure() / 25600
-    let altitude: number = weatherbit.altitude()
-    serial.writeValue("temperature", temperature)
-    serial.writeValue("humidity", humidity)
-    serial.writeValue("pressure", pressure)
-    serial.writeValue("altitude", altitude)
+    signal = serial.readLine()
+    temperature = weatherbit.temperature() / 100
+    humidity = weatherbit.humidity() / 1024
+    pressure = weatherbit.pressure() / 25600
+    altitude = weatherbit.altitude()
+    windSpeed = weatherbit.windSpeed()
+    windDirection = weatherbit.windDirection()
+    serial.writeValue("temperature:", temperature)
+    serial.writeValue(",humidity:", humidity)
+    serial.writeValue(",pressure:", pressure)
+    serial.writeValue(",altitude:", altitude)
+    serial.writeValue(",wind speed:", windSpeed)
+    serial.writeString(",wind direction:" + windDirection)
     led.setBrightness(10)
     basic.showLeds(`
         . . . . .
@@ -34,9 +57,3 @@ basic.forever(function () {
     basic.clearScreen()
     basic.pause(1000)
 })
-serial.redirectToUSB()
-/*serial.redirect(
-SerialPin.P15,
-SerialPin.P14,
-BaudRate.BaudRate9600
-)*/
